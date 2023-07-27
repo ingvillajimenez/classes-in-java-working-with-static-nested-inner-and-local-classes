18f387c213f731f178763dd1de9cf2b243b942d8
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-27 14:04:01 -0600

    Navigating the Drawbacks of Static Nested Classes

--- NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
+++ NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Main.java
@@ -5,31 +5,46 @@ public class Main {
     public static void main(String[] args) {
 
         Product keyboard = new Product(
-                "Dell Keyboard", 49, "Computer Accessories", 4.3);
-        Product mouse = new Product(
-                "HP Mouse", 19, "Computer Accessories", 3.9);
-        Product printer = new Product(
-                "Epson Printer", 599, "Electronics", 4.5);
+                "Dell Keyboard", 49, 4.3);
+        keyboard.getReviews().add("Very good!");
 
-        keyboard.getRating().printRating();
-        mouse.getRating().printRating();
-        printer.getRating().printRating();
+        Product mouse = new Product(
+                "HP Mouse", 19, 3.9);
+
+        Product printer = new Product(
+                "Epson Printer", 599, 4.5);
+        printer.getReviews().add("Works smoothly");
+
+        System.out.println(keyboard);
+        System.out.println(mouse);
+        System.out.println(printer);
 
         System.out.println();
 
-        Product.Rating rating = new Product.Rating(3.6, keyboard);
-        rating.printRating();
-
-//        Product keyboard = new Product("Dell Keyboard", 49, "Computer Accessories");
-//        Product mouse = new Product("HP Mouse", 19, "Computer Accessories");
-//        Product printer = new Product("Epson Printer", 599, "Electronics");
-
-//        keyboard.getCategory().printProductDetails();
-//        mouse.getCategory().printProductDetails();
-//        printer.getCategory().printProductDetails();
+//        Product.ReviewsAndRatings reviewsAndRatings1 = new Product.ReviewsAndRatings(4.5, null); // java.lang.IllegalArgumentException
+//        Product.ReviewsAndRatings reviewsAndRatings2 = new Product.ReviewsAndRatings(2.5, null); // java: com.skillsoft.nestedclasseslambdas.Product.ReviewsAndRatings has private access in com.skillsoft.nestedclasseslambdas.Product
+//
+//        System.out.println(reviewsAndRatings1);
+//        System.out.println(reviewsAndRatings2);
 //
 //        System.out.println();
 
+//        Product keyboard = new Product(
+//                "Dell Keyboard", 49, 4.3);
+//        keyboard.getReviewsAndRatings().addReviews("Very good!");
+//
+//        Product mouse = new Product(
+//                "HP Mouse", 19, 3.9);
+//
+//        Product printer = new Product(
+//                "Epson Printer", 599, 4.5);
+//        printer.getReviewsAndRatings().addReviews("Works smoothly");
+//
+//        System.out.println(keyboard);
+//        System.out.println(mouse);
+//        System.out.println(printer);
+//
+//        System.out.println();
     }
 }
 
--- NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Product.java
+++ NestedClassesAndLambdas/src/com/skillsoft/nestedclasseslambdas/Product.java
@@ -1,98 +1,70 @@
 package com.skillsoft.nestedclasseslambdas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Product {
 
     private long id;
     private String name;
     private int price;
 
-    private Category category;
-    private Rating rating;
+    private ReviewsAndRatings reviewsAndRatings;
 
-    public Product(String name, int price, String categoryName, double rating) {
+    public Product(String name, int price, double rating) {
 
         this.id = Math.round(Math.random() * 10000);
         this.name = name;
         this.price = price;
 
-//        this.category = new Category(categoryName);
-        this.category = new Category(categoryName, this);
-        this.rating = new Rating(rating, this);
+        this.reviewsAndRatings = new ReviewsAndRatings(rating, this);
     }
 
-    public void updateCategory(String name) {
-        this.category.updateCategory(name);
+    public double getRatings() {
+        return reviewsAndRatings.rating;
     }
 
-    public Category getCategory() {
-        return category;
+    public List<String> getReviews() {
+        return reviewsAndRatings.reviews;
     }
 
-    public Rating getRating() {
-        return rating;
-    }
+//    public ReviewsAndRatings getReviewsAndRatings() {
+//        return reviewsAndRatings;
+//    }
 
     @Override
     public String toString() {
-        return String.format("Product ID: %d, Name: %s, Price: %d, Category name: %s",
-                id, name, price, category.name);
+        return String.format("Name: %s, Price: %d, Reviews and ratings: %s",
+                name, price, reviewsAndRatings);
     }
 
-    // NOTE: The "public" modifier means that objects of this class
-    // can be instantiated outside of this class as well
-    public static class Category {
+//    public static class ReviewsAndRatings {
+    private static class ReviewsAndRatings {
 
-        private final long id;
-        private String name;
-        private Product product;
+        private final Product product;
+        private final double rating;
+        private final List<String> reviews;
 
-        public Category(String name, Product product) {
-            this.id = Math.round(Math.random() * 10000);
-            this.name = name;
+        public ReviewsAndRatings(double rating, Product product) {
+            // throwing exception from constructors is bad practice in Java
+//            if (product == null) {
+//                throw new IllegalArgumentException("The input argument 'product' cannot be null");
+//            }
+
             this.product = product;
+            this.rating = rating;
+
+            this.reviews = new ArrayList<>();
         }
 
-        public void updateCategory(String name) {
-            this.name = name;
-        }
-
-//        public void printProductDetails() {
-//            // Category is not automatically associated with a product instance
-//            System.out.println(price); // Non-static field 'price' cannot be referenced from a static context
-//            System.out.println(this.price); // Cannot resolve symbol 'price'
-//            System.out.println(Category.this.price); // Cannot resolve symbol 'price'
-//            System.out.println(Product.this.price); // 'com.skillsoft.nestedclasseslambdas.Product.this' cannot be referenced from a static context
-//        }
-
-        public void printProductDetails() {
-            System.out.println("**** Product details");
-            System.out.println("ID: " + product.id);
-            System.out.println("Name: " + product.name);
-            System.out.println("Price: " + product.price);
-            System.out.println("Category: " + product.category);
+        public void addReviews(String review) {
+            this.reviews.add(review);
         }
 
         @Override
         public String toString() {
-            return String.format("Category ID: %d, Name: %s", id, name);
-        }
-    }
-
-    public static class Rating {
-
-        private Product product;
-        private double rating;
-
-        public Rating(double rating, Product product) {
-
-            this.product = product;
-            this.rating = rating;
-        }
-
-        public void printRating() {
-            System.out.println("****Average product rating");
-            System.out.println(String.format("Product ID: %d Name: %s Rating: %.2f\n",
-                    product.id, product.name, rating));
+            return String.format("Product: %s, rating: %.1f, reviews: %s",
+                    product == null ? "<unknown>" : product.name, rating, reviews);
         }
     }
 }
